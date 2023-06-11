89475da93bc989218db3862eb815b743b4ec688c
krunal timbadiya <98510266+KRUNALTIMBADIYA@users.noreply.github.com>
2023-06-11 22:38:59 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,29 +38,29 @@ $(document).ready(function () {
     });
 
     // <!-- emailjs to mail contact form data -->
-function sendMail() {
-  var params = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    phone: document.getElementById("phone").value,
-    message: document.getElementById("message").value,
-  };
+// function sendMail() {
+//   var params = {
+//     name: document.getElementById("name").value,
+//     email: document.getElementById("email").value,
+//     phone: document.getElementById("phone").value,
+//     message: document.getElementById("message").value,
+//   };
 
-  const serviceID = "service_996ck9d";
-  const templateID = "template_rqnp04b";
+//   const serviceID = "service_996ck9d";
+//   const templateID = "template_rqnp04b";
+shtml
+//     emailjs.send(serviceID, templateID, params)
+//     .then(res=>{
+//         document.getElementById("name").value = "";
+//         document.getElementById("email").value = "";
+//         document.getElementById("message").value = "";
+//         console.log(res);
+//         alert("Your message sent successfully!!")
 
-    emailjs.send(serviceID, templateID, params)
-    .then(res=>{
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("message").value = "";
-        console.log(res);
-        alert("Your message sent successfully!!")
-
-    })
-    .catch(err=>console.log(err));
- alert("Your message not sent successfully!!")
-}
+//     })
+//     .catch(err=>console.log(err));
+//  alert("Your message not sent successfully!!")
+// }
 
     // <!-- emailjs to mail contact form data -->
 
