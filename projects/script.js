7fed80b53943c8f058b3365a110ed983e272c12c
krunal timbadiya <98510266+KRUNALTIMBADIYA@users.noreply.github.com>
2023-06-11 19:29:09 +0530

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -135,3 +135,26 @@ document.onkeydown = function (e) {
         return false;
     }
 }
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_996ck9d";
+  const templateID = "template_rqnp04b";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+         document.getElementById("phone").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
